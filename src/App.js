e3fe764765d6457683bc102701daa15930597734
JWS93 <justinscott820@gmail.com>
2024-10-31 14:49:39 -0700

    navbar structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import ConversionTable from './ConversionTable'
+import CurrencyCalculator from './CurrencyCalculator'
 
-const App () {
+function App () {
   return (
-    <ConversionTable />
-  )
+    <Router>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <NavLink to="/" className="navbar-brand" >
+          <FontAwesomeIcon icon="fa-solid fa-money-bill-transfer" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+          Currency Converter
+          </NavLink>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" component={Home} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
